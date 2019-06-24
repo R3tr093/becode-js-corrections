fd3648fb232576045e0e626f0997c22ed1c08172
Mossiat Jeoffrey <mossiat.jeoffrey@outlook.com>
2019-06-24 16:37:07 +0200

    Done
    Done

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,6 +9,60 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+    
+
 (() => {
-    // your code here
+    // to get the value of an input: document.getElementById("element-id").value
+
+    
+
+        
+        // lors du click sur le bouton run
+    
+        document.getElementById("run").addEventListener("click", () => {
+        
+     
+          // On récupère différentes valeurs sélectionner par l'utilisateur
+            
+          let day = document.getElementById("dob-day").value;
+    
+          let month = document.getElementById("dob-month").value;
+
+          let year = document.getElementById("dob-year").value;
+    
+            
+          // On crée une date, et on stocke le jour, mois, année actuel    
+          
+          let date = new Date();
+
+          let currentDay = date.getDate();
+            
+          let currentMonth = date.getMonth();
+            
+          let currentYear = date.getFullYear();
+
+            
+        // On compare les données récuperer pour déterminer l'age de l'utilisateur selon sa date de naissance et la date d'aujourd'hui.
+            
+         if (month > currentMonth)
+             {
+                 year++
+             }
+
+        if (month === currentMonth)
+            {
+                if (day > currentDay)
+                    {
+                        year++
+                    }
+            }
+
+            
+             alert(currentYear - year) 
+        
+        
+    });
+    
+  
 })();
